7d46c7a2f7d5c8b77b563740d0136d72a1bd27d1
swomf <swomf@proton.me>
2026-07-26 23:42:13 -0400

    gtk: point to cursors called Blackbriar, not Blackbriar-cursors

--- src/gtk/build_gtk.py
+++ src/gtk/build_gtk.py
@@ -3,9 +3,17 @@
 from .apps import transform_apps
 from .colors import transform_colors
 from .decorations import transform_decorations
+from ..common import replace_exact
 
 
 def build_gtk(root):
+    # upstream install.sh derives "Blackbriar-cursors"
+    # but we vendor the Qogir theme as "Blackbriar"
+    replace_exact(
+        root / "install.sh",
+        "CursorTheme=${2}-cursors",
+        "CursorTheme=Blackbriar",
+    )
     transform_colors(root / "src/sass/_colors.scss")
     transform_decorations(root / "src/sass/gtk/_common-3.0.scss")
     transform_apps(root / "src/sass/gtk/apps/_misc.scss")
