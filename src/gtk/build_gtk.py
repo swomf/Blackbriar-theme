0fbc7ae73298d8bf7ee7cfe341a3198c31273d08
swomf <swomf@proton.me>
2026-07-30 02:24:31 -0400

    gtk: fix gtk2.0 0F0F0F and wrong libadwaita link
    force symlink libadwaita wasnt setup until
    9e5fd1881bbebece26bd0f05729f662bd1236c75
    so i only noticed that it was symlinking to
    THEME_NAME=Graphite instead of THEME_NAME=Blackbriar now.

--- src/gtk/build_gtk.py
+++ src/gtk/build_gtk.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from .apps import transform_apps
-from .colors import transform_colors
 from .decorations import transform_decorations
 from ..common import replace_exact
 
@@ -14,6 +13,13 @@ def build_gtk(root):
         "CursorTheme=${2}-cursors",
         "CursorTheme=Blackbriar",
     )
-    transform_colors(root / "src/sass/_colors.scss")
+    # installer affects gtk2.0
+    replace_exact(root / "install.sh", "#0F0F0F", "#000000")
+    # symlink shouldnt lead to Graphite
+    replace_exact(root / "install.sh", "THEME_NAME=Graphite", "THEME_NAME=Blackbriar")
+    # basically the mother color file
+    for old in ("#030303", "#0F0F0F", "#121212"):
+        replace_exact(root / "src/sass/_colors.scss", old, "#000000")
+
     transform_decorations(root / "src/sass/gtk/_common-3.0.scss")
     transform_apps(root / "src/sass/gtk/apps/_misc.scss")
--- src/gtk/colors.py
+++
@@ -1,6 +0,0 @@
-from ..common import replace_exact
-
-
-def transform_colors(path):
-    for old in ("#030303", "#0F0F0F", "#121212"):
-        replace_exact(path, old, "#000000")
