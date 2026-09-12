d7980899f14bede5b01d4701017e73e406dc629b
swomf <swomf@proton.me>
2026-09-12 17:36:07 -0400

    gtk: fix upstream gtk4 black-on-black listview text

--- src/gtk/build_gtk.py
+++ src/gtk/build_gtk.py
@@ -2,7 +2,7 @@
 
 from .apps import transform_apps, transform_gnome
 from .decorations import transform_decorations
-from ..common import replace_exact
+from ..common import block, replace_exact
 
 
 def build_gtk(root):
@@ -20,6 +20,48 @@ def build_gtk(root):
     # basically the mother color file
     for old in ("#030303", "#0F0F0F", "#121212"):
         replace_exact(root / "src/sass/_colors.scss", old, "#000000")
+
+    # test on ./gtk/build-gtk4/demos/gtk-demo/gtk4-demo
+    # unselected sidebar test was black onb lack because
+    # Graphite's listview assumes light accent background
+    # ----> we remove Graphite's special row setup
+    replace_exact(
+        root / "src/sass/gtk/_common-4.0.scss",
+        block(
+            """
+              @if $sidebar =='styled' {
+                &.navigation-sidebar {
+                  @if $topbar =='dark' and $variant =='light' {
+                    margin: 0;
+                    background-color: transparent;
+                    color: $titlebar-text-secondary;
+                  }
+
+                  @else {
+                    background-color: $sidebg;
+                    color: on($sidebg);
+                    margin: 0 0 $menu-radius 0;
+                    border-radius: 0 $menu-radius $menu-radius 0;
+                  }
+
+                  > row {
+                    @extend %sidebar-row;
+
+                    button {
+                      @extend %sidebar-flat-button;
+                    }
+                  }
+
+                  .dim-label {
+                    color: on($sidebg, disabled);
+                  }
+                }
+              }
+            """,
+            indent=2,
+        ),
+        "",
+    )
 
     transform_decorations(root / "src/sass/gtk/_common-3.0.scss")
     transform_apps(root / "src/sass/gtk/apps/_misc.scss")
